80cc9f33d1ccbbb8516dd85393f0f9692047471a
i3games <i3games@users.noreply.github.com>
2021-07-23 00:55:04 +0200

    cylinder reaction

--- src/index.js
+++ src/index.js
@@ -43,6 +43,7 @@ AFRAME.registerComponent('wallet', {
       const address = await wallet.getPKH();
       console.log(wallet);
       console.log(address);
+      if (wallet && address) { this.setAttribute('color', '#00ff00'); } else { this.setAttribute('color', '#ff0000'); }
     });
   }
 });
